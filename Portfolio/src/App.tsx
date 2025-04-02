40112a806b91c331799900d78c31018de65debde
DESKTOP-17PTUJM\Jeshen <concepcionjessa321@gmail.com>
2025-04-02 15:08:15 +0800

    UPDATE: Initial files

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,35 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { UpOutlined } from "@ant-design/icons";
+import { Button, Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-scroll";
+import AboutSection from "./components/About";
+import ContactSection from "./components/Contact";
+import HeroSection from "./components/Home";
+import NavBar from "./components/NavBar";
+import ProjectSection from "./components/Projects";
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const [isButtonFixed, setIsButtonFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.scrollY + window.innerHeight;
+
+      if (scrollPosition >= scrollHeight - 50) {
+        setIsButtonFixed(true);
+      } else {
+        setIsButtonFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Layout>
+      <NavBar />
+      <Content style={{ padding: "0 50px" }}>
+        <section
+          id="hero"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <HeroSection />
+        </section>
 
-export default App
+        <hr />
+
+        <section
+          id="about"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <AboutSection />
+        </section>
+
+        <hr />
+
+        <section
+          id="projects"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            background: "#9E339F",
+          }}
+        >
+          <ProjectSection />
+        </section>
+
+        <hr />
+
+        <section
+          id="contact"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <ContactSection />
+        </section>
+      </Content>
+
+      <div style={{ position: "relative" }}>
+        <Link to="hero" smooth={true} duration={500} activeClass="active">
+          <Button
+            icon={<UpOutlined />}
+            size="large"
+            style={{
+              position: isButtonFixed ? "fixed" : "absolute",
+              bottom: isButtonFixed ? "20px" : "20px",
+              right: "20px",
+              color: "#9E339F",
+              backgroundColor: "#f5f5f5",
+              borderRadius: "50%",
+              boxShadow: "0px 8px 8px rgba(0, 0, 0, 0.5)",
+              zIndex: 9999,
+            }}
+          />
+        </Link>
+      </div>
+
+      <Footer style={{ textAlign: "center", backgroundColor: "#9e339f" }}>
+        Jessa Concepcion ©{new Date().getFullYear()}
+        <div
+          style={{
+            display: "flex",
+            gap: "50px",
+            marginTop: "15px",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="https://www.linkedin.com/in/jessa-concepcion-64b184284"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <FaLinkedin
+              style={{
+                fontSize: "30px",
+                color: " #fff",
+                transition: "color 0.3s;",
+              }}
+            />
+          </a>
+          <a
+            href="https://github.com/jeshen321"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <FaGithub
+              style={{
+                fontSize: "30px",
+                color: " #fff",
+                transition: "color 0.3s;",
+              }}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/_jeshen"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <FaInstagram
+              style={{
+                fontSize: "30px",
+                color: " #fff",
+                transition: "color 0.3s;",
+              }}
+            />
+          </a>
+        </div>
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
--- Portfolio/src/main.tsx
+++ Portfolio/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider
+      theme={{
+        token: {
+          //#BF94E4
+          colorBgBase: "#F5F5F5",
+          colorTextBase: "#fff",
+          fontFamily: "Courier New",
+          fontSize: 15,
+          colorPrimary: "#fff",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </StrictMode>
+);
